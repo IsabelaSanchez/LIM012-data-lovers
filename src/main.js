413b9053f327695f0530b0495e3d2b36eecd1c32
IsabelaSanchez <natyst_26@hotmail.com>
2020-03-27 08:55:05 -0500

    Historia 3/Filtro de roles

--- src/main.js
+++ src/main.js
@@ -2,46 +2,102 @@ import { example } from './data.js';
 // import data from './data/atletas/atletas.js';
  import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
-
-//CONSOLE LOGS DE PRUEBA
-//esta es la rama de Isabela
-//esta es nuestra rama
-//holaaaa
+/*Todo los campeones*/
 let stringTemplate = '';
 const container=document.getElementById('container');
-
-for (const prop in data.data){
+const dataTodosLosCampeones=data.data;
+for (const mostrar in dataTodosLosCampeones){
   stringTemplate+= `
   <div id='box'>
-  <img src=${data.data[prop].img}/>
-  <p id='name'>${data.data[prop].name}</p>
-  <p id='title'>${data.data[prop].title}</p>
+  <img id='image'src=${dataTodosLosCampeones[mostrar].img}/>
+  <p><button id='name'>${dataTodosLosCampeones[mostrar].name}</button></p>
+  <p id='title'>${dataTodosLosCampeones[mostrar].title}</p>
   </div>
   `
 };
 container.innerHTML= stringTemplate;
+/*Filtro Tank*/
 
+let stringTemplateTank='';
+const containerTank=document.getElementById('containerTank');
 
-/*console.log(example);
-console.log(data);
-console.log(data['type']);
-console.log(data['data'['Aatrox']]);
-console.log(data.data['Aatrox']); //LO LOGRE LLAME SOLO A UN CAMPEÓN
-console.log(data.data.Aatrox);
-console.log(data.data.Aatrox.img);
-console.log(data.data.Aatrox.name);
-console.log(data.data.Aatrox.title);
-console.log(data.data.Aatrox.tags[0]);
-console.log(data.data.Aatrox.tags[1]);
+for (const mostrarTank in dataTodosLosCampeones){
+  if (dataTodosLosCampeones[mostrarTank].tags.indexOf("Tank") != -1){
+    stringTemplateTank+= `
+    <div id='box'>
+    <img src=${dataTodosLosCampeones[mostrarTank].img}/>
+    <p><button id='name'>${dataTodosLosCampeones[mostrarTank].name}</button></p>
+    <p id='title'>${dataTodosLosCampeones[mostrarTank].title}</p>
+    </div>`
+}};
+containerTank.innerHTML= stringTemplateTank;
 
-DATA QUE ESTOY USANDO AHORA
-document.getElementById('Aatrox-name').innerHTML=data.data.Aatrox.name;
-document.getElementById('Aatrox-title').innerHTML=data.data.Aatrox.title;
-document.getElementById('Ahri-name').innerHTML=data.data.Ahri.name;
-document.getElementById('Ahri-title').innerHTML=data.data.Ahri.title;
-document.getElementById('Akali-name').innerHTML=data.data.Akali.name;
-document.getElementById('Akali-title').innerHTML=data.data.Akali.title;
-document.getElementById('Alistar-name').innerHTML=data.data.Alistar.name;
-document.getElementById('Alistar-title').innerHTML=data.data.Alistar.title;
-document.getElementById('Amumu-name').innerHTML=data.data.Amumu.name;
-document.getElementById('Amumu-title').innerHTML=data.data.Amumu.title;*/
+let stringTemplateMage='';
+const containerMage=document.getElementById('containerMage');
+
+for (const mostrarMage in dataTodosLosCampeones){
+  if (dataTodosLosCampeones[mostrarMage].tags.indexOf("Mage") != -1){
+    stringTemplateMage+= `
+    <div id='box'>
+    <img src=${dataTodosLosCampeones[mostrarMage].img}/>
+    <p><button id='name'>${dataTodosLosCampeones[mostrarMage].name}</button></p>
+    <p id='title'>${dataTodosLosCampeones[mostrarMage].title}</p>
+    </div>`
+}};
+containerMage.innerHTML= stringTemplateMage;
+
+let stringTemplateAssassin='';
+const containerAssassin=document.getElementById('containerAssassin');
+
+for (const mostrarAssassin in dataTodosLosCampeones){
+  if (dataTodosLosCampeones[mostrarAssassin].tags.indexOf("Assassin") != -1){
+    stringTemplateAssassin+= `
+    <div id='box'>
+    <img src=${dataTodosLosCampeones[mostrarAssassin].img}/>
+    <p><button id='name'>${dataTodosLosCampeones[mostrarAssassin].name}</button></p>
+    <p id='title'>${dataTodosLosCampeones[mostrarAssassin].title}</p>
+    </div>`
+}};
+containerAssassin.innerHTML= stringTemplateAssassin;
+
+let stringTemplateFighter='';
+const containerFighter=document.getElementById('containerFighter');
+
+for (const mostrarFighter in dataTodosLosCampeones){
+  if (dataTodosLosCampeones[mostrarFighter].tags.indexOf("Fighter") != -1){
+    stringTemplateFighter+= `
+    <div id='box'>
+    <img src=${dataTodosLosCampeones[mostrarFighter].img}/>
+    <p><button id='name'>${dataTodosLosCampeones[mostrarFighter].name}</button></p>
+    <p id='title'>${dataTodosLosCampeones[mostrarFighter].title}</p>
+    </div>`
+}};
+containerFighter.innerHTML= stringTemplateFighter;
+
+let stringTemplateMarksman='';
+const containerMarksman=document.getElementById('containerMarksman');
+
+for (const mostrarMarksman in dataTodosLosCampeones){
+  if (dataTodosLosCampeones[mostrarMarksman].tags.indexOf("Marksman") != -1){
+    stringTemplateMarksman+= `
+    <div id='box'>
+    <img src=${dataTodosLosCampeones[mostrarMarksman].img}/>
+    <p><button id='name'>${dataTodosLosCampeones[mostrarMarksman].name}</button></p>
+    <p id='title'>${dataTodosLosCampeones[mostrarMarksman].title}</p>
+    </div>`
+}};
+containerMarksman.innerHTML= stringTemplateMarksman;
+
+let stringTemplateSupport='';
+const containerSupport=document.getElementById('containerSupport');
+
+for (const mostrarSupport in dataTodosLosCampeones){
+  if (dataTodosLosCampeones[mostrarSupport].tags.indexOf("Support") != -1){
+    stringTemplateSupport+= `
+    <div id='box'>
+    <img src=${dataTodosLosCampeones[mostrarSupport].img}/>
+    <p><button id='name'>${dataTodosLosCampeones[mostrarSupport].name}</button></p>
+    <p id='title'>${dataTodosLosCampeones[mostrarSupport].title}</p>
+    </div>`
+}};
+containerSupport.innerHTML= stringTemplateSupport;
